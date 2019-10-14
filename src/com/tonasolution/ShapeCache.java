8153beb94c67966b8da9e3fbc6a54aaf452344f8
zouhaire <zouhaire@idside.de>
2019-10-14 16:20:52 +0100

    impl shape prototyping

---
+++ src/com/tonasolution/ShapeCache.java
@@ -0,0 +1,28 @@
+package com.tonasolution;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+
+public class ShapeCache {
+    private static Hashtable<String, Shape> shapeMap = new Hashtable<>();
+
+    public static Shape getShape(String shapeId){
+        Shape cachedShape = shapeMap.get(shapeId);
+        return (Shape) cachedShape.clone();
+    }
+
+    public static void loadCache(){
+        Shape circle = new Circle();
+        circle.setId("1");
+        shapeMap.put(circle.getId(), circle);
+
+        Shape square = new Square();
+        square.setId("2");
+        shapeMap.put(square.getId(), square);
+
+        Shape rectangle = new Rectangle();
+        rectangle.setId("3");
+        shapeMap.put(rectangle.getId(), rectangle);
+    }
+
+}
